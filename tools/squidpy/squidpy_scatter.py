d3ed1e2a4be8a311e52e48972a2245f68bc51278
CameronFRWatson <watsocam@ohsu.edu>
2024-07-09 14:35:34 -0700

    finalize spatial scatter code, add test data

--- tools/squidpy/squidpy_scatter.py
+++ tools/squidpy/squidpy_scatter.py
@@ -3,40 +3,45 @@ import ast
 import json
 import warnings
 
-import pandas as pd
 import squidpy as sq
 from anndata import read_h5ad
 
 
-def main(inputs, anndata, output, output_plot):
+def main(inputs, output_plot):
+
     """
-    Parameter
-    ---------
     inputs : str
-        File path to galaxy tool parameter.
-    anndata : str
-        File path to anndata containing phenotyping info.
-    output : str
-        File path to output.
-    output_plot: str or None
-        File path to save the plotting image.
+        File path to galaxy tool JSON inputs config file
+    output_plot: str
+        File path to save the plotting image
     """
     warnings.simplefilter('ignore')
 
+    # read inputs JSON
     with open(inputs, 'r') as param_handler:
         params = json.load(param_handler)
 
-    adata = read_h5ad(anndata)
+    # collapse param dict hierarchy, parse inputs
+    plot_opts = params.pop('plot_opts')
+    legend_opts = params.pop('legend_opts')
+    aes_opts = params.pop('aesthetic_opts')
+    options = {**params, **plot_opts, **legend_opts, **aes_opts}
 
+    # read input anndata file
+    adata_fh = options.pop('anndata')
+    adata = read_h5ad(adata_fh)
+
+    # ensure spatial coords in anndata.obsm['spatial']
+    # if not, populate with user provided X/Y coord column names
+    x, y = options.pop('x_coord'), options.pop('y_coord')
     if 'spatial' not in adata.obsm:
         try:
-            adata.obsm['spatial'] = adata.obs[
-                ['X_centroid', 'Y_centroid']].values
+            adata.obsm['spatial'] = adata.obs[[x, y]].values
         except Exception as e:
             print(e)
 
-    options = params['analyses']['options']
-
+    # scan thru tool params,
+    # replace None values, and reformat specific parameters
     for k, v in options.items():
         if not isinstance(v, str):
             continue
@@ -45,56 +50,38 @@ def main(inputs, anndata, output, output_plot):
             options[k] = None
             continue
 
-        if k == 'genes':    # for spatial_autocorr and sepal
+        if k == 'groups':
             options[k] = [e.strip() for e in v.split(',')]
-        elif k == 'radius':    # for spatial_neighbors
+
+        elif k == 'crop_coord':
+            # split str on commas into tuple of coords
+            # then nest in list (expected by squidpy function)
+            options[k] = [tuple([int(e.strip()) for e in v.split(',')])]
+
+        elif k == 'figsize':
             options[k] = ast.literal_eval(v)
-        elif k == 'interactions':    # for ligrec
-            options[k] = pd.read_csv(v, sep="\t")
-        elif k == 'max_neighs':
-            options[k] = int(v)      # for sepal
 
-    if output_plot:
-        plotting_options = params['analyses']['plotting_options']
-        for k, v in plotting_options.items():
-            if not isinstance(v, str):
-                continue
+    # not exposing this parameter for now. Only useful to change for ST data
+    # and can otherwise just be problematic.
+    # Explicitly setting to None is necessary to avoid an error
+    options['shape'] = None
 
-            if v in ('', 'none'):
-                plotting_options[k] = None
-                continue
-
-            if k == 'figsize':
-                options[k] = ast.literal_eval(v)
-            elif k in ('palette', 'score', 'source_groups', 'target_groups'):
-                options[k] = [e.strip() for e in v.split(',')]
-            elif k == 'means_range':        # ligrec
-                v = v.strip()
-                if v[0] == '(':
-                    v = v[1:]
-                if v[-1] == ')':
-                    v = v[:-1]
-                options[k] = tuple([float(e.strip()) for e in v.split(',', 1)])
-
+    # call squidpy spatial scatter function, unpack tool params
     sq.pl.spatial_scatter(
         adata=adata,
-        shape=options['shape'],
-        color=options['color'],
-        groups=options['groups'],
-        use_raw=options['use_raw'],
-        size=options['size'],
-        # need to figure out scale and scalebar
+        save='image.png',
+        **options
     )
 
 
 if __name__ == '__main__':
+
     aparser = argparse.ArgumentParser()
-    aparser.add_argument("-i", "--inputs", dest="inputs", required=True)
-    aparser.add_argument("-e", "--output", dest="output", required=True)
-    aparser.add_argument("-a", "--anndata", dest="anndata", required=True)
+    aparser.add_argument(
+        "-i", "--inputs", dest="inputs", required=True)
     aparser.add_argument(
         "-p", "--output_plot", dest="output_plot", required=False)
 
     args = aparser.parse_args()
 
-    main(args.inputs, args.anndata, args.output, args.output_plot)
+    main(args.inputs, args.output_plot)
